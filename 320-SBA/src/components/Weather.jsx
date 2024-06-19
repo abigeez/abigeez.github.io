a0f7e7106cb083a9c7cb00976c6ec513f1253bf1
Abi Garner <abigail.gee@hotmail.com>
2024-06-19 15:14:00 -0400

    importing weather icons to use for weather search bar component

--- 320-SBA/src/components/Weather.jsx
+++ 320-SBA/src/components/Weather.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import './Weather.css'
 import search_icon from '../assets/mgGlass.png'
+import light_rain from '../assets/lightrain.png'
+import partly_cloudly from '../assets/partlyCloudy.png'
+import snow_fall from '../assets/snowfall.png'
+import search_icon from '../assets/mgGlass.png'
+import search_icon from '../assets/mgGlass.png'
+import search_icon from '../assets/mgGlass.png'
+import search_icon from '../assets/mgGlass.png'
+
 const Weather = () => {
   return (
     <div className="weatherOne">
@@ -8,6 +16,7 @@ const Weather = () => {
         <input type='text' placeholder='Search'/>
         <img className="magGlass"src={search_icon} alt="" /> 
        </div>
+       <img src="" alt="" />
     </div>
 
   )
