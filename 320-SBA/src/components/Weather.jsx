d2bde969a002e72245d2b6892a14a3c8af8074fb
Abi Garner <abigail.gee@hotmail.com>
2024-06-19 15:38:41 -0400

    adding new icons img and description for test

--- 320-SBA/src/components/Weather.jsx
+++ 320-SBA/src/components/Weather.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import './Weather.css'
 import search_icon from '../assets/mgGlass.png'
 import light_rain from '../assets/lightrain.png'
-import partly_cloudly from '../assets/partlyCloudy.png'
+import partly_cloudly from '../assets/partlyCloudly.png'
 import snow_fall from '../assets/snowfall.png'
-import search_icon from '../assets/mgGlass.png'
-import search_icon from '../assets/mgGlass.png'
-import search_icon from '../assets/mgGlass.png'
-import search_icon from '../assets/mgGlass.png'
+import snow_flake from '../assets/snowflake.png'
+import rain_shower from '../assets/rainShowers.png'
+import storm_icon from '../assets/storm.png'
+import sun_icon from '../assets/sun.png'
 
 const Weather = () => {
   return (
     <div className="weatherOne">
-       <div className="searchBar">
-        <input type='text' placeholder='Search'/>
-        <img className="magGlass"src={search_icon} alt="" /> 
-       </div>
-       <img src="" alt="" />
+      <div className="searchBar">
+        <input type='text' placeholder='Search' />
+        <img className="magGlass" src={search_icon} alt="" />
+      </div>
+    <img className="weather-icon" src={sun_icon} alt=""/>
+      <p>70°F</p>
+      <p>Madrid</p>
     </div>
 
   )
