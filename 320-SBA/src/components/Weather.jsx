5a83b089d9b762a7b0b48db05259e6d4cad67b06
Abi Garner <abigail.gee@hotmail.com>
2024-06-19 00:52:41 -0400

    adding search symbol

--- 320-SBA/src/components/Weather.jsx
+++ 320-SBA/src/components/Weather.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Weather.css'
-
+import search_icon from '../assets/mgGlass.png'
 const Weather = () => {
   return (
     <div className="weatherOne">
        <div className="searchBar">
         <input type='text' placeholder='Search'/>
-        <img src="" alt="" /> 
+        <img src={search_icon} alt="" /> 
        </div>
     </div>
 
