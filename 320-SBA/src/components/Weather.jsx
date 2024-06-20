ea726387f88e016b245839b718059df5146dc0dc
Abi Garner <abigail.gee@hotmail.com>
2024-06-20 14:29:50 -0400

    deleting .env to conceal api key

--- 320-SBA/src/components/Weather.jsx
+++ 320-SBA/src/components/Weather.jsx
@@ -15,7 +15,7 @@ const Weather = () => {
 
   const searchAPI = async (city) => {
     try {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_ID}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=eaa170d0202a3df084b6117bc8ccb889`;
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
