c653d9219ca1f074c05caa6224f5fb2a327b78f7
Abi Garner <abigail.gee@hotmail.com>
2024-06-20 03:13:32 -0400

    useEffect hook for when data is requested and snet back

--- 320-SBA/src/components/Weather.jsx
+++ 320-SBA/src/components/Weather.jsx
@@ -18,6 +18,7 @@ const Weather = () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_ID}`;
         const response = await fetch(url);
         const data = await response.json();
+        console.log(data);
       } catch (error) {
     }
   }
